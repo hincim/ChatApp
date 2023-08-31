5126fb6823619ba3f050e0780c66c487099eca7f
Hakan İnciman <hakanincim4@gmail.com>
2023-08-31 21:51:52 +0300

    sign in  page added

--- app/src/main/java/com/example/myapplication/activities/SignInActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/SignInActivity.kt
@@ -1,19 +1,30 @@
 package com.example.myapplication.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Binder
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivitySignInBinding
+import com.example.myapplication.utilities.Constants
+import com.example.myapplication.utilities.PreferenceManager
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var preferenceManager: PreferenceManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        preferenceManager = PreferenceManager(this)
+        if (preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setListeners()
@@ -25,23 +36,67 @@ class SignInActivity : AppCompatActivity() {
                 SignUpActivity::class.java))
         })
 
-        binding.buttonSignIn.setOnClickListener { v -> addDataToFirestore() }
+        binding.buttonSignIn.setOnClickListener {
+            if (isValidSignInDetails()){
+                signIn()
+            }
+        }
     }
 
-    private fun addDataToFirestore(){
-        val database: FirebaseFirestore = FirebaseFirestore.getInstance()
-        val data = hashMapOf(
-            "first_name" to "hinfo",
-            "last_name" to "craft"
-        )
-        database.collection("users")
-            .add(data)
-            .addOnSuccessListener {
-                Toast.makeText(this,"Data Inserted",Toast.LENGTH_SHORT).show()
+    private fun signIn(){
+
+        loading(true)
+        val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+        db.collection(Constants.KEY_COLLECTION_USERS)
+            .whereEqualTo(Constants.KEY_EMAIL,binding.inputEmail.text.toString())
+            .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.text.toString())
+            .get()
+            .addOnCompleteListener {
+                if (it.isSuccessful && it.result != null && it.result.documents.size > 0){
+                    val snapshot: DocumentSnapshot = it.result.documents[0]
+                    preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true)
+                    preferenceManager.putString(Constants.KEY_USER_ID, snapshot.id)
+                    preferenceManager.putString(Constants.KEY_NAME, snapshot.getString(Constants.KEY_NAME))
+                    preferenceManager.putString(Constants.KEY_IMAGE, snapshot.getString(Constants.KEY_IMAGE))
+                    val intent = Intent(this,MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(intent)
+                }else{
+                    loading(false)
+                    showToast("Unable to sign in")
+                }
             }
-            .addOnFailureListener {
-                Toast.makeText(this,it.message,Toast.LENGTH_SHORT).show()
-            }
+    }
+
+    private fun loading(isLoading: Boolean){
+
+        if (isLoading){
+            binding.buttonSignIn.visibility = View.INVISIBLE
+            binding.progressBar.visibility = View.VISIBLE
+        }else{
+            binding.buttonSignIn.visibility = View.VISIBLE
+            binding.progressBar.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isValidSignInDetails(): Boolean{
+
+        return if (binding.inputEmail.text.toString().trim().isEmpty()){
+            showToast("Enter email")
+            false
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.text.toString()).matches()){
+            showToast("Enter valid email")
+            false
+        }else if (binding.inputPassword.text.toString().trim().isEmpty()){
+            showToast("Enter password")
+            false
+        }else{
+            true
+        }
     }
 }
 
